a7d4d602595224cd9b9b9f6e3e52cdef558ce153
Kerem <a.kerem7celik@gmail.com>
2022-10-10 23:27:24 +0300

    init:app command

---
+++ app/Console/Commands/InitAppCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class InitAppCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Init App';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('migrate:fresh');
+        Artisan::call('passport:install');
+        Artisan::call('db:seed');
+    }
+}
