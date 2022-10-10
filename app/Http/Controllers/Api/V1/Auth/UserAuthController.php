ef2bd566862b80ceee23121d7598b1a220b37be2
Kerem <a.kerem7celik@gmail.com>
2022-10-10 23:00:39 +0300

    refresh-token endpoint

--- app/Http/Controllers/Api/V1/Auth/UserAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/UserAuthController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Auth\UserLoginRequest;
+use App\Http\Requests\Api\V1\Auth\UserRefreshTokenRequest;
 use App\Http\Requests\Api\V1\Auth\UserRegisterRequest;
 use App\Http\Resources\Api\V1\UserResource;
 use App\Services\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
@@ -31,9 +34,36 @@ class UserAuthController extends Controller
             if (!auth()->attempt($data))
                 return response()->json('Email or password is wrong', 401);
 
-            $token = auth()->user()->createToken('API Token')->accessToken;
+            $client = DB::table('oauth_clients')->where('provider', 'users')->first();
+            $request = Request::create('/oauth/token', 'POST',[
+                'grant_type' => 'password',
+                'client_id' => $client->id,
+                'client_secret' => $client->secret,
+                'username' => $data['email'],
+                'password' => $data['password'],
+                'scope' => ''
+            ]);
 
-            return response()->json(['token' => $token]);
+            return app()->handle($request);
+        } catch (\Throwable $exception) {
+            throw $exception;
+        }
+    }
+
+    public function refreshToken(UserRefreshTokenRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $client = DB::table('oauth_clients')->where('provider', 'users')->first();
+            $request = Request::create('/oauth/token', 'POST',[
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $data['refresh_token'],
+                'client_id' => $client->id,
+                'client_secret' => $client->secret,
+                'scope' => ''
+            ]);
+
+            return app()->handle($request);
         } catch (\Throwable $exception) {
             throw $exception;
         }
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::post('register', [UserAuthController::class, 'register']);
     Route::post('login', [UserAuthController::class, 'login']);
+    Route::post('refresh-token', [UserAuthController::class, 'refreshToken']);
     Route::middleware('auth:api')->group(function() {
         Route::post('logout', [UserAuthController::class, 'logout']);
         Route::get('user', [UserController::class, 'user']);
